26c932f027490bc0031ce3b14294a087aa83ae05
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2024-12-14 17:49:48 +0300

    Update CommentMoreButton.tsx

--- src/components/comments/CommentMoreButton.tsx
+++ src/components/comments/CommentMoreButton.tsx
@@ -37,7 +37,7 @@ export default function CommentMoreButton({
           <DropdownMenuItem onClick={() => setShowDeleteDialog(true)}>
             <span className="flex items-center gap-3 text-destructive">
               <Trash2 className="size-4" />
-              Delete
+              Jê bibe
             </span>
           </DropdownMenuItem>
         </DropdownMenuContent>
