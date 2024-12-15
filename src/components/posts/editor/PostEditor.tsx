b2fe7e15bb7058ac0f2e39e8efe4341a806725c2
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2024-12-16 02:07:46 +0300

    Update PostEditor.tsx

--- src/components/posts/editor/PostEditor.tsx
+++ src/components/posts/editor/PostEditor.tsx
@@ -45,7 +45,7 @@ export default function PostEditor() {
         italic: false,
       }),
       Placeholder.configure({
-        placeholder: "What's crack-a-lackin'?",
+        placeholder: "Selam aleykum hevalê/a hêja ,kerem bike tişteki parve bike",
       }),
     ],
   });
