a36ea36e5bd01a20ec66aedcb61e5c483666aa93
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2024-12-18 02:51:10 +0300

    Update page.tsx

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -19,9 +19,10 @@ export default function Page() {
     <main className="flex h-screen items-center justify-center p-5">
       <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
         <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
-          <h5 className="text-center text-3xl font-bold">Bi Xêr Hatî</h5>
+          <h5 className="text-center text-3xl font-bold"> Ser nameya namê ,namê Allah<br></<br>
+ Bê nameye namê kêmî wî Wallah"</h5>
          
-          <h1 className="text-center text-3xl font-bold">Gotin</h1>
+          <h1 className="text-center text-3xl font-bold">Lebê ?</h1>
 
           <div className="space-y-5">
             <GoogleSignInButton />
