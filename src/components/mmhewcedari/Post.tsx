6221359a3e12d30050ee94b27b2abdf2987eb245
cigerxwinislam <islamtunc748@gmail.com>
2025-01-13 16:48:50 -0500

    serxere nu

--- src/components/mmhewcedari/Post.tsx
+++ src/components/mmhewcedari/Post.tsx
@@ -6,16 +6,12 @@ import { useSession } from "@/app/(main)/SessionProvider";
 import { PostData } from "@/lib/types";
 import { cn, formatRelativeDate } from "@/lib/utils";
 import { Media } from "@prisma/client";
-import { MessageSquare } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import Comments from "../comments/Comments";
 import Linkify from "../Linkify";
 import UserAvatar from "../UserAvatar";
 import UserTooltip from "../UserTooltip";
-import BookmarkButton from "./BookmarkButton";
-import LikeButton from "./LikeButton";
 import PostMoreButton from "./PostMoreButton";
 
 interface PostProps {
@@ -68,30 +64,8 @@ export default function Post({ post }: PostProps) {
         <MediaPreviews attachments={post.attachments} />
       )}
       <hr className="text-muted-foreground" />
-      <div className="flex justify-between gap-5">
-        <div className="flex items-center gap-5">
-          <LikeButton
-            postId={post.id}
-            initialState={{
-              likes: post._count.likes,
-              isLikedByUser: post.likes.some((like) => like.userId === user.id),
-            }}
-          />
-          <CommentButton
-            post={post}
-            onClick={() => setShowComments(!showComments)}
-          />
-        </div>
-        <BookmarkButton
-          postId={post.id}
-          initialState={{
-            isBookmarkedByUser: post.bookmarks.some(
-              (bookmark) => bookmark.userId === user.id,
-            ),
-          }}
-        />
-      </div>
-      {showComments && <Comments post={post} />}
+     
+    
     </article>
   );
 }
@@ -147,19 +121,3 @@ function MediaPreview({ media }: MediaPreviewProps) {
   return <p className="text-destructive">Ev medya nabe</p>;
 }
 
-interface CommentButtonProps {
-  post: PostData;
-  onClick: () => void;
-}
-
-function CommentButton({ post, onClick }: CommentButtonProps) {
-  return (
-    <button onClick={onClick} className="flex items-center gap-2">
-      <MessageSquare className="size-5" />
-      <span className="text-sm font-medium tabular-nums">
-        {post._count.comments}{" "}
-        <span className="hidden sm:inline">Şîroveyên</span>
-      </span>
-    </button>
-  );
-}
