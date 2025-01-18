b4960191c97fac7862244249e1303e59c4e88ea6
cigerxwinislam <islamtunc748@gmail.com>
2025-01-18 09:11:50 -0500

    serxere nu

--- src/components/mmhuner/editor/mm/mm.tsx
+++ src/components/mmhuner/editor/mm/mm.tsx
@@ -16,12 +16,48 @@ export default function MmmmmmHome() {
 
         <Tabs defaultValue="mm">
           <TabsList>
-            <TabsTrigger value="for-you">Ev</TabsTrigger>
-            <TabsTrigger value="following">Arsa</TabsTrigger>
+            <TabsTrigger value="for-you">Otomobil</TabsTrigger>
+            <TabsTrigger value="following">Arazi , Suv, Pickup</TabsTrigger>
+            <TabsTrigger value="for-you">Motosiklet</TabsTrigger>
+            <TabsTrigger value="following">Minivan ve Panelvan</TabsTrigger>
+            <TabsTrigger value="for-you">Ticari Araçlar</TabsTrigger>
+            <TabsTrigger value="following">Elektrikli Araçlar</TabsTrigger>
+            
+            
             
             
           </TabsList>
          
+
+
+
+
+          <TabsList>
+            <TabsTrigger value="for-you">Elektrikli Araçlar</TabsTrigger>
+            <TabsTrigger value="following">Deniz Araçları</TabsTrigger>
+            <TabsTrigger value="for-you">Hasarlı Araçları</TabsTrigger>
+            <TabsTrigger value="following">Karavan</TabsTrigger>
+            <TabsTrigger value="for-you">Ticari Araçlar</TabsTrigger>
+            <TabsTrigger value="following">Elektrikli Araçlar</TabsTrigger>
+            
+            
+            
+            
+          </TabsList>
+
+        
+
+
+          <TabsList>
+            <TabsTrigger value="for-you">Klasik Araçlar</TabsTrigger>
+            <TabsTrigger value="following">Hava  Araçları</TabsTrigger>
+            <TabsTrigger value="for-you">ATV </TabsTrigger>
+            <TabsTrigger value="following">UTV</TabsTrigger>
+           
+            
+            
+            
+          </TabsList>
           <TabsContent value="for-you">
          
          <MmHome/>
