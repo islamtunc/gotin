0e0ecd96c08411522179b1ffb2a5285787096c81
islamtunc <islamtunc748@gmail.com>
2025-07-24 19:00:14 +0300

    elhamdulillahirabbulalemin

--- mela/src/app/api/users/[userId]/posts/route.ts
+++ mela/src/app/api/users/[userId]/posts/route.ts
@@ -8,40 +8,34 @@
 
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
-import { getPostDataInclude, PostsPage } from "@/lib/types";
-import { NextRequest } from "next/server";
+import { getUserDataSelect } from "@/lib/types";
 
 export async function GET(
-  req: NextRequest,
-  { params: { userId } }: { params: { userId: string } },
+  req: Request,
+  { params: { username } }: { params: { username: string } },
 ) {
   try {
-    const cursor = req.nextUrl.searchParams.get("cursor") || undefined;
+    const { user: loggedInUser } = await validateRequest();
 
-    const pageSize = 10;
-
-    const { user } = await validateRequest();
-
-    if (!user) {
+    if (!loggedInUser) {
       return Response.json({ error: "Unauthorized" }, { status: 401 });
     }
 
-    const posts = await prisma.post.findMany({
-      where: { userId },
-      include: getPostDataInclude(user.id),
-      orderBy: { createdAt: "desc" },
-      take: pageSize + 1,
-      cursor: cursor ? { id: cursor } : undefined,
+    const user = await prisma.user.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: "insensitive",
+        },
+      },
+      select: getUserDataSelect(loggedInUser.id),
     });
 
-    const nextCursor = posts.length > pageSize ? posts[pageSize].id : null;
+    if (!user) {
+      return Response.json({ error: "User not found" }, { status: 404 });
+    }
 
-    const data: PostsPage = {
-      posts: posts.slice(0, pageSize),
-      nextCursor,
-    };
-
-    return Response.json(data);
+    return Response.json(user);
   } catch (error) {
     console.error(error);
     return Response.json({ error: "Internal server error" }, { status: 500 });
