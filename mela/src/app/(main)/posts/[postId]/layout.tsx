b972c73e42b0ac3ec77537279689a9bd32fc7680
Yekazad Software Center <91857222+islamtunc@users.noreply.github.com>
2025-08-07 15:04:18 +0300

    SuphanAllah ve Bihamdihi

---
+++ mela/src/app/(main)/posts/[postId]/layout.tsx
@@ -0,0 +1,28 @@
+//Bismillahirrahmanirahim 
+//Elhamdulillahirabbulalemin 
+// Es selatu vesselamu ala rasulina Muhammed'in ve ala alihi ve sahbihi ecmain.
+//SuphanAllah ul Azim ve Bihamdihi
+//Allah U Ekber velillahilhamd
+//La ilahe illAllah u vahdehu la şerike leh,lehul mulku ve lehul hamdu
+//Ve huwe ala kulli şey in kadir
+
+import ChatProviderClient from "../ChatProviderClient"
+
+export default  function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+
+  return (
+
+
+<ChatProviderClient/>
+
+{children}
+
+<ChatProviderClient/>
+
+  );
+}
